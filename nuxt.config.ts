ed0b44d1431f3504ce932546e9faf7c4feb74bd4
Steven <steven.csdi.hsieh@acer.com>
2024-11-18 10:34:11 +0800

    github page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@unocss/nuxt'],
-  css: ['@unocss/reset/tailwind-compat.css']
+  css: ['@unocss/reset/tailwind-compat.css'],
+  app: {
+    baseURL: '/chrismass/' // 例如 '/my-project/'
+  }
 })
